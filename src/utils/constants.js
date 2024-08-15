ad5e96205eb308af55254e96854a0fcf9de557cc
Harshit Tripathi <htripathi.4444@gmail.com>
2024-08-16 02:14:16 +0530

    without backend

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,5 @@
 const GOOGLE_API_KEY = "AIzaSyA1XOE5eVU8GICR8_txi24KeBIYq8hlVd8";
 
-//const CORS_REMOVER="https://thingproxy.freeboard.io/fetch/"
-
 export const OFFSET_LIVE_CHAT = 25;
 
 const CORS_REMOVER="https://thingproxy.freeboard.io/fetch/"
